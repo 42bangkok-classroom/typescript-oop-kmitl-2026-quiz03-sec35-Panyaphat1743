ecbad2d18c24e723b8b6bb87c65c59d1b7047579
panyaphat insawng <68011743@kmitl.ac.th>
2026-02-16 10:16:38 +0700

    p03

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -1,5 +1,30 @@
 import { Product } from './product'
 
 export class Playstation extends Product {
+  playname:string
+  gen:string
+  price:number
+
+  constructor(name:string,generation:number,price:number){
+    super(name)
+    this.playname = name
+    this.gen = 'Gen' + ' ' + generation
+    this.price = price
+  }
+  getProfile(){
+    return this.playname + ' ' + '(' + this.gen + ')'
+  }
+  getPrice(){
+    return this.price
+  }
+  getDiscountPrice(){
+    return this.price = this.price - (DISCOUNT_PERCENT)
+  }
+
+
+
+
+
+
   // TODO: implement class properties, constructor with super(...), and methods
 }
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,4 +1,17 @@
 export class Product {
-    readonly name?:string
+    readonly name:string
+    protected price?:number = 0
+    static readonly DISCOUNT_PERCENT:number = 10
+    constructor(name:string,price?:number){
+        this.name = name
+        this.price = price
+    }
+
+    getPrice(){
+        return this.price
+    }
+    setPrice(price: number){
+        return this.price = price
+    }
 }
 
