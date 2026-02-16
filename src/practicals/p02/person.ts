3f83a335c5dfe2b418adb089b87a97b2ed235420
panyaphat insawng <68011743@kmitl.ac.th>
2026-02-16 09:42:34 +0700

    p02

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,16 @@
-export class Person {}
+export class Person {
+    private age?:number
+    firstname?:string
+    lastname?:string
+    static COUNTRY:string = 'Thailand'
+
+    getFullName(){
+        return this.firstname + " " + this.lastname
+    }
+    setAge(age:number){
+        this.age = age
+    }
+    getAge(){
+        return this.age
+    }
+}
