4d0077ff1717e21d750ba62396c0429d8b237ffe
panyaphat insawng <68011743@kmitl.ac.th>
2026-03-02 20:38:05 +0700

    p03.9

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -2,18 +2,18 @@ import { Product } from './product'
 
 export class Playstation extends Product {
   playname:string
-  gen:string
+  generation:string
   price:number
   constructor(name:string,generation:number,price:number){
     super(name)
     this.playname = name
-    this.gen = 'Gen ' + generation
+    this.generation = 'Gen ' + generation
     this.price = price
   }
 
 
   getProfile(){
-    return this.playname + ' ' + '('  + this.gen + ')'
+    return this.playname + ' ' + '('  + this.generation + ')'
   }
 
   getDiscountPrice(){
