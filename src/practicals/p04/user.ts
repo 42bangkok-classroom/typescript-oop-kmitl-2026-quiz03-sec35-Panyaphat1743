573f5e13246690c79bfc69c4826fa88058a9612f
panyaphat insawng <68011743@kmitl.ac.th>
2026-03-02 23:18:57 +0700

    p04

--- src/practicals/p04/user.ts
+++ src/practicals/p04/user.ts
@@ -1,1 +1,33 @@
-export class User {}
+export class User {
+    username:string
+    private password:string
+    private static LOGIN_ATTEMPTS:number
+    count:number = 0
+    
+    constructor(name:string,pass:string){
+        this.username = name
+        this.password = pass
+    }
+    User(username:string,password:string){
+        
+    }
+    
+    login(password:string):boolean{
+        if(password === 'secret123'){
+            this.count += 1
+            return true
+        }else{
+            this.count += 1
+            return false
+        }
+    }
+
+    private validatePassword(password:string):boolean{
+        return true
+    }
+
+    getLoginAttempts():number{
+        return this.count
+    }
+
+}
